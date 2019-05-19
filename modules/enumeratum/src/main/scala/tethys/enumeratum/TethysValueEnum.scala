d85fc620514fd213c542515948c9bb7d31496424
Boris <boris@f0w.org>
2019-05-19 05:24:39 +0300

    Add enumeratum support

---
+++ modules/enumeratum/src/main/scala/tethys/enumeratum/TethysValueEnum.scala
@@ -0,0 +1,45 @@
+package tethys.enumeratum
+
+import _root_.enumeratum.values._
+import tethys.readers.KeyReader
+import tethys.writers.KeyWriter
+import tethys.{JsonReader, JsonWriter}
+
+sealed trait TethysValueEnum[ValueType, EntryType <: ValueEnumEntry[ValueType]] { _: ValueEnum[ValueType, EntryType] =>
+  implicit def tethysReader: JsonReader[EntryType]
+  implicit def tethysWriter: JsonWriter[EntryType]
+}
+
+trait StringTethysEnum[E <: StringEnumEntry] extends TethysValueEnum[String, E] { _: ValueEnum[String, E] =>
+  implicit val tethysReader: JsonReader[E] = Enumeratum.valueReader(this)
+  implicit val tethysWriter: JsonWriter[E] = Enumeratum.valueWriter(this)
+  implicit val tethysKeyReader: KeyReader[E] = Enumeratum.keyReader(this)(_.withValueOpt)
+  implicit val tethysKeyWriter: KeyWriter[E] = Enumeratum.keyWriter(_.value)
+}
+
+trait IntTethysEnum[E <: IntEnumEntry] extends TethysValueEnum[Int, E] { _: ValueEnum[Int, E] =>
+  implicit val tethysReader: JsonReader[E] = Enumeratum.valueReader(this)
+  implicit val tethysWriter: JsonWriter[E] = Enumeratum.valueWriter(this)
+}
+
+trait LongTethysEnum[E <: LongEnumEntry] extends TethysValueEnum[Long, E] { _: ValueEnum[Long, E] =>
+  implicit val tethysReader: JsonReader[E] = Enumeratum.valueReader(this)
+  implicit val tethysWriter: JsonWriter[E] = Enumeratum.valueWriter(this)
+}
+
+trait ShortTethysEnum[E <: ShortEnumEntry] extends TethysValueEnum[Short, E] { _: ValueEnum[Short, E] =>
+  implicit val tethysReader: JsonReader[E] = Enumeratum.valueReader(this)
+  implicit val tethysWriter: JsonWriter[E] = Enumeratum.valueWriter(this)
+}
+
+// FIXME Add decoders
+/*
+trait CharTethysEnum[E <: CharEnumEntry] extends TethysValueEnum[Char, E] { _: ValueEnum[Char, E] =>
+  implicit val tethysReader: JsonReader[E] = Enumeratum.valueReader(this)
+  implicit val tethysWriter: JsonWriter[E] = Enumeratum.valueWriter(this)
+}
+
+trait ByteTethysEnum[E <: ByteEnumEntry] extends TethysValueEnum[Byte, E] { _: ValueEnum[Byte, E] =>
+  implicit val tethysReader: JsonReader[E] = Enumeratum.valueReader(this)
+  implicit val tethysWriter: JsonWriter[E] = Enumeratum.valueWriter(this)
+}*/
