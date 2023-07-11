f38e7e390a4528d8366e497412dab76482462a23
Scala Steward <scala_steward@virtuslab.com>
2023-07-11 02:33:42 +0000

    Reformat with scalafmt 3.7.8
    Executed command: scalafmt --non-interactive

--- modules/core/src/main/scala/tethys/package.scala
+++ modules/core/src/main/scala/tethys/package.scala
@@ -8,59 +8,86 @@ import scala.Specializable.Group
 
 package object tethys {
 
-  final val specializations = new Group((Short, Int, Long, Float, Double, Boolean))
+  final val specializations = new Group(
+    (Short, Int, Long, Float, Double, Boolean)
+  )
 
   // given
 
   implicit class JsonWriterOps[A](val a: A) extends AnyVal {
-    def asJson(implicit jsonWriter: JsonWriter[A], tokenWriterProducer: TokenWriterProducer): String = {
+    def asJson(implicit
+        jsonWriter: JsonWriter[A],
+        tokenWriterProducer: TokenWriterProducer
+    ): String = {
       val stringWriter = new StringWriter()
       writeJson(tokenWriterProducer.forWriter(stringWriter))
       stringWriter.toString
     }
 
-    def asJsonWith(jsonWriter: JsonWriter[A])(implicit tokenWriterProducer: TokenWriterProducer): String = {
+    def asJsonWith(
+        jsonWriter: JsonWriter[A]
+    )(implicit tokenWriterProducer: TokenWriterProducer): String = {
       asJson(jsonWriter, tokenWriterProducer)
     }
 
-    def writeJson(tokenWriter: TokenWriter)(implicit jsonWriter: JsonWriter[A]): Unit = {
-      try jsonWriter.write(a, tokenWriter) finally {
+    def writeJson(
+        tokenWriter: TokenWriter
+    )(implicit jsonWriter: JsonWriter[A]): Unit = {
+      try jsonWriter.write(a, tokenWriter)
+      finally {
         tokenWriter.flush()
       }
     }
   }
 
   implicit class WriterOps(val w: Writer) extends AnyVal {
-    def toTokenWriter(implicit tokenWriterProducer: TokenWriterProducer): TokenWriter = tokenWriterProducer.forWriter(w)
+    def toTokenWriter(implicit
+        tokenWriterProducer: TokenWriterProducer
+    ): TokenWriter = tokenWriterProducer.forWriter(w)
   }
 
   implicit class StringReaderOps(val json: String) extends AnyVal {
-    def jsonAs[A](implicit jsonReader: JsonReader[A], producer: TokenIteratorProducer): Either[ReaderError, A] = {
+    def jsonAs[A](implicit
+        jsonReader: JsonReader[A],
+        producer: TokenIteratorProducer
+    ): Either[ReaderError, A] = {
       new StringReader(json).readJson[A]
     }
 
-    def toTokenIterator(implicit producer: TokenIteratorProducer): Either[ReaderError, TokenIterator] = {
+    def toTokenIterator(implicit
+        producer: TokenIteratorProducer
+    ): Either[ReaderError, TokenIterator] = {
       new StringReader(json).toTokenIterator
     }
   }
 
   implicit class ReaderReaderOps(val reader: Reader) extends AnyVal {
-    def readJson[A](implicit jsonReader: JsonReader[A], producer: TokenIteratorProducer): Either[ReaderError, A] = {
+    def readJson[A](implicit
+        jsonReader: JsonReader[A],
+        producer: TokenIteratorProducer
+    ): Either[ReaderError, A] = {
       implicit val root: FieldName = FieldName()
       producer.fromReader(reader).right.flatMap(_.readJson[A])
     }
 
-    def readJsonWith[A](jsonReader: JsonReader[A])(implicit producer: TokenIteratorProducer): Either[ReaderError, A] = {
+    def readJsonWith[A](
+        jsonReader: JsonReader[A]
+    )(implicit producer: TokenIteratorProducer): Either[ReaderError, A] = {
       readJson[A](jsonReader, producer)
     }
 
-    def toTokenIterator(implicit producer: TokenIteratorProducer): Either[ReaderError, TokenIterator] = {
+    def toTokenIterator(implicit
+        producer: TokenIteratorProducer
+    ): Either[ReaderError, TokenIterator] = {
       producer.fromReader(reader)
     }
   }
 
-  implicit class TokenIteratorOps(val tokenIterator: TokenIterator) extends AnyVal {
-    def readJson[A](implicit jsonReader: JsonReader[A]): Either[ReaderError, A] = {
+  implicit class TokenIteratorOps(val tokenIterator: TokenIterator)
+      extends AnyVal {
+    def readJson[A](implicit
+        jsonReader: JsonReader[A]
+    ): Either[ReaderError, A] = {
       implicit val fieldName: FieldName = FieldName()
       ReaderError.catchNonFatal(jsonReader.read(tokenIterator))
     }
--- modules/macro-derivation/src/main/scala-3/tethys/derivation/SemiautoDerivation.scala
+++ modules/macro-derivation/src/main/scala-3/tethys/derivation/SemiautoDerivation.scala
@@ -11,7 +11,10 @@ import tethys.derivation.builder.{
   WriterDerivationConfig,
   WriterDescription
 }
-import tethys.derivation.impl.builder.{ReaderDescriptionMacro, WriterDescriptionMacro}
+import tethys.derivation.impl.builder.{
+  ReaderDescriptionMacro,
+  WriterDescriptionMacro
+}
 import tethys.derivation.impl.derivation.SemiautoDerivationMacro
 import scala.quoted.*
 import scala.annotation.compileTimeOnly
@@ -20,31 +23,47 @@ trait SemiautoDerivation {
   inline def jsonWriter[T]: JsonObjectWriter[T] =
     ${ SemiautoDerivation.jsonWriter[T] }
 
-  inline def jsonWriter[T](inline description: WriterDescription[T]): JsonObjectWriter[T] =
+  inline def jsonWriter[T](
+      inline description: WriterDescription[T]
+  ): JsonObjectWriter[T] =
     ${ SemiautoDerivation.jsonWriterWithDescription[T]('description) }
 
-  inline def jsonWriter[T <: Product](inline builder: => WriterBuilder[T]): JsonObjectWriter[T] =
+  inline def jsonWriter[T <: Product](
+      inline builder: => WriterBuilder[T]
+  ): JsonObjectWriter[T] =
     ${ SemiautoDerivation.jsonWriterWithBuilder[T]('builder) }
 
-  inline def jsonWriter[T](inline config: WriterDerivationConfig): JsonObjectWriter[T] =
+  inline def jsonWriter[T](
+      inline config: WriterDerivationConfig
+  ): JsonObjectWriter[T] =
     ${ SemiautoDerivation.jsonWriterWithConfig[T]('config) }
 
-  inline def describe[T <: Product](inline builder: => WriterBuilder[T]): WriterDescription[T] =
+  inline def describe[T <: Product](
+      inline builder: => WriterBuilder[T]
+  ): WriterDescription[T] =
     ${ SemiautoDerivation.describeWriter[T]('builder) }
 
   inline def jsonReader[T]: JsonReader[T] =
     ${ SemiautoDerivation.jsonReader[T] }
 
-  inline def jsonReader[T](inline description: ReaderDescription[T]): JsonReader[T] =
+  inline def jsonReader[T](
+      inline description: ReaderDescription[T]
+  ): JsonReader[T] =
     ${ SemiautoDerivation.jsonReaderWithDescription[T]('description) }
 
-  inline def jsonReader[T](inline config: ReaderDerivationConfig): JsonReader[T] =
+  inline def jsonReader[T](
+      inline config: ReaderDerivationConfig
+  ): JsonReader[T] =
     ${ SemiautoDerivation.jsonReaderWithConfig[T]('config) }
 
-  inline def jsonReader[T <: Product](inline builder: => ReaderBuilder[T]): JsonReader[T] =
+  inline def jsonReader[T <: Product](
+      inline builder: => ReaderBuilder[T]
+  ): JsonReader[T] =
     ${ SemiautoDerivation.jsonReaderWithBuilder[T]('builder) }
 
-  inline def describe[T <: Product](inline builder: => ReaderBuilder[T]): ReaderDescription[T] =
+  inline def describe[T <: Product](
+      inline builder: => ReaderBuilder[T]
+  ): ReaderDescription[T] =
     ${ SemiautoDerivation.describeReader[T]('builder) }
 
   implicit class ReaderFieldStringOps(val s: String) {
@@ -62,34 +81,52 @@ private[this] object SemiautoDerivation {
   def jsonWriter[T: Type](using Quotes): Expr[JsonObjectWriter[T]] =
     new SemiautoDerivationMacro(quotes).simpleJsonWriter[T]
 
-  def jsonWriterWithConfig[T: Type](config: Expr[WriterDerivationConfig])(using Quotes): Expr[JsonObjectWriter[T]] =
-    new SemiautoDerivationMacro(quotes).jsonWriterWithConfig[T](config)
-
-  def jsonWriterWithDescription[T: Type](description: Expr[WriterDescription[T]])(using
+  def jsonWriterWithConfig[T: Type](config: Expr[WriterDerivationConfig])(using
       Quotes
   ): Expr[JsonObjectWriter[T]] =
-    new SemiautoDerivationMacro(quotes).jsonWriterWithWriterDescription[T](description)
+    new SemiautoDerivationMacro(quotes).jsonWriterWithConfig[T](config)
 
-  def jsonWriterWithBuilder[T <: Product: Type](builder: Expr[WriterBuilder[T]])(using
+  def jsonWriterWithDescription[T: Type](
+      description: Expr[WriterDescription[T]]
+  )(using
+      Quotes
+  ): Expr[JsonObjectWriter[T]] =
+    new SemiautoDerivationMacro(quotes)
+      .jsonWriterWithWriterDescription[T](description)
+
+  def jsonWriterWithBuilder[T <: Product: Type](
+      builder: Expr[WriterBuilder[T]]
+  )(using
       Quotes
   ): Expr[JsonObjectWriter[T]] =
     new SemiautoDerivationMacro(quotes).jsonWriterWithBuilder[T](builder)
 
-  def describeWriter[T <: Product: Type](builder: Expr[WriterBuilder[T]])(using Quotes): Expr[WriterDescription[T]] =
+  def describeWriter[T <: Product: Type](builder: Expr[WriterBuilder[T]])(using
+      Quotes
+  ): Expr[WriterDescription[T]] =
     new WriterDescriptionMacro(quotes).simpleDescription[T](builder)
 
   def jsonReader[T: Type](using Quotes): Expr[JsonReader[T]] =
     new SemiautoDerivationMacro(quotes).simpleJsonReader[T]
 
-  def jsonReaderWithConfig[T: Type](config: Expr[ReaderDerivationConfig])(using Quotes): Expr[JsonReader[T]] =
+  def jsonReaderWithConfig[T: Type](config: Expr[ReaderDerivationConfig])(using
+      Quotes
+  ): Expr[JsonReader[T]] =
     new SemiautoDerivationMacro(quotes).jsonReaderWithConfig[T](config)
 
-  def jsonReaderWithDescription[T: Type](description: Expr[ReaderDescription[T]])(using Quotes): Expr[JsonReader[T]] =
-    new SemiautoDerivationMacro(quotes).jsonReaderWithReaderDescription[T](description)
+  def jsonReaderWithDescription[T: Type](
+      description: Expr[ReaderDescription[T]]
+  )(using Quotes): Expr[JsonReader[T]] =
+    new SemiautoDerivationMacro(quotes)
+      .jsonReaderWithReaderDescription[T](description)
 
-  def jsonReaderWithBuilder[T <: Product: Type](builder: Expr[ReaderBuilder[T]])(using Quotes): Expr[JsonReader[T]] =
+  def jsonReaderWithBuilder[T <: Product: Type](
+      builder: Expr[ReaderBuilder[T]]
+  )(using Quotes): Expr[JsonReader[T]] =
     new SemiautoDerivationMacro(quotes).jsonReaderWithBuilder[T](builder)
 
-  def describeReader[T <: Product: Type](builder: Expr[ReaderBuilder[T]])(using Quotes): Expr[ReaderDescription[T]] =
+  def describeReader[T <: Product: Type](builder: Expr[ReaderBuilder[T]])(using
+      Quotes
+  ): Expr[ReaderDescription[T]] =
     new ReaderDescriptionMacro(quotes).simpleDescription[T](builder)
 }
