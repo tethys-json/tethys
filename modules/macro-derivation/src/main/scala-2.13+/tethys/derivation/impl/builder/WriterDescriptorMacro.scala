5cbcafb8aa25bb66ca2bbda18a05fb11a42161a3
Scala Steward <scala_steward@virtuslab.com>
2023-07-16 22:44:59 +0000

    Reformat with scalafmt 3.7.10
    Executed command: scalafmt --non-interactive

--- modules/macro-derivation/src/main/scala-2.13+/tethys/derivation/impl/builder/WriterDescriptorMacro.scala
+++ modules/macro-derivation/src/main/scala-2.13+/tethys/derivation/impl/builder/WriterDescriptorMacro.scala
@@ -4,14 +4,16 @@ import tethys.derivation.builder.{WriterBuilder, WriterDescription}
 
 import scala.reflect.macros.blackbox
 
-/**
-  * Created by eld0727 on 23.04.17.
+/** Created by eld0727 on 23.04.17.
   */
-class WriterDescriptorMacro(val c: blackbox.Context) extends WriterBuilderCommons {
+class WriterDescriptorMacro(val c: blackbox.Context)
+    extends WriterBuilderCommons {
 
   import c.universe._
 
-  def simpleDescription[A: WeakTypeTag](builder: Expr[WriterBuilder[A]]): Expr[WriterDescription[A]] = {
+  def simpleDescription[A: WeakTypeTag](
+      builder: Expr[WriterBuilder[A]]
+  ): Expr[WriterDescription[A]] = {
     convertWriterBuilder[A](builder)
   }
 
--- modules/refined/src/main/scala/tethys/refined/TethysRefinedInstances.scala
+++ modules/refined/src/main/scala/tethys/refined/TethysRefinedInstances.scala
@@ -6,26 +6,31 @@ import tethys.readers.tokens.TokenIterator
 import tethys.{JsonReader, JsonWriter}
 
 trait TethysRefinedInstances {
-  implicit final def RefinedJsonWriter[T: JsonWriter, P, F[_, _]: RefType]: JsonWriter[F[T, P]] =
+  implicit final def RefinedJsonWriter[T: JsonWriter, P, F[_, _]: RefType]
+      : JsonWriter[F[T, P]] =
     JsonWriter[T].contramap(RefType[F].unwrap)
 
   implicit final def RefinedJsonReader[T: JsonReader, P, F[_, _]: RefType](
-    implicit validate: Validate[T, P]
+      implicit validate: Validate[T, P]
   ): JsonReader[F[T, P]] =
     new JsonReader[F[T, P]] {
-      override def read(it: TokenIterator)(implicit fieldName: FieldName): F[T, P] =
+      override def read(it: TokenIterator)(implicit
+          fieldName: FieldName
+      ): F[T, P] =
         fromEither(RefType[F].refine(JsonReader[T].read(it)))
     }
 
-  implicit final def RefinedKeyReader[T, P, F[_, _]: RefType](
-    implicit reader: KeyReader[T],
-    validate: Validate[T, P]
+  implicit final def RefinedKeyReader[T, P, F[_, _]: RefType](implicit
+      reader: KeyReader[T],
+      validate: Validate[T, P]
   ): KeyReader[F[T, P]] = new KeyReader[F[T, P]] {
     override def read(s: String)(implicit fieldName: FieldName): F[T, P] =
       fromEither(RefType[F].refine(reader.read(s)))
   }
 
-  private def fromEither[A](either: Either[String, A])(implicit fieldName: FieldName): A =
+  private def fromEither[A](
+      either: Either[String, A]
+  )(implicit fieldName: FieldName): A =
     either match {
       case Right(value) => value
       case Left(err)    => ReaderError.wrongJson(s"Refined error: $err")
