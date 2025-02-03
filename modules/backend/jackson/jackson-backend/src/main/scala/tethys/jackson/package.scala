e6903eddc5722516f164379a36261c539daf934a
Georgii Kovalev <goshacodes@gmail.com>
2025-02-03 11:44:47 +0500

    refactor to prepare for another backend

--- modules/backend/jackson/jackson-backend/src/main/scala/tethys/jackson/package.scala
+++ modules/backend/jackson/jackson-backend/src/main/scala/tethys/jackson/package.scala
@@ -1,36 +1,81 @@
 package tethys
 
-import java.io.{Reader, Writer}
-
-import com.fasterxml.jackson.core.JsonFactory
+import java.io.Reader
+import com.fasterxml.jackson.core.{
+  JsonFactory,
+  JsonFactoryBuilder,
+  JsonGenerator
+}
 import tethys.readers.{FieldName, ReaderError}
 import tethys.readers.tokens.{TokenIterator, TokenIteratorProducer}
-import tethys.writers.tokens.{TokenWriter, TokenWriterProducer}
+import tethys.writers.tokens.{
+  TokenWriter,
+  TokenWriterConfig,
+  TokenWriterProducer
+}
 
 package object jackson {
-  lazy val defaultJsonFactory: JsonFactory = {
-    val f = new JsonFactory()
-    f.configure(JsonFactory.Feature.INTERN_FIELD_NAMES, false)
-    f
+  lazy val defaultJsonFactory: JsonFactory =
+    new JsonFactoryBuilder()
+      .configure(JsonFactory.Feature.INTERN_FIELD_NAMES, false)
+      .build()
+
+  class JacksonTokenWriterProducer(
+      jsonFactory: JsonFactory,
+      // used for compatibility where tethys.jackson.pretty import is required
+      modifyConfig: TokenWriterConfig => TokenWriterConfig
+  ) extends TokenWriterProducer {
+
+    private def updateGeneratorFromConfig(
+        generator: JsonGenerator,
+        config: TokenWriterConfig
+    ): JsonGenerator = {
+      if (config == TokenWriterConfig.default)
+        generator
+      else {
+        val first =
+          if (config.indentionStep == 2)
+            generator.useDefaultPrettyPrinter()
+          else
+            generator
+
+        val second =
+          if (config.escapeUnicode)
+            first.setHighestNonEscapedChar(127)
+          else
+            first
+        second
+      }
+    }
+
+    override def produce(config: TokenWriterConfig): TokenWriter = {
+      val generator = updateGeneratorFromConfig(
+        jsonFactory.createGenerator(new java.io.StringWriter()),
+        modifyConfig(config)
+      )
+      new JacksonTokenWriter(generator)
+    }
   }
 
   implicit def jacksonTokenWriterProducer(implicit
       jsonFactory: JsonFactory = defaultJsonFactory
-  ): TokenWriterProducer = new TokenWriterProducer {
-    override def forWriter(writer: Writer): TokenWriter = {
-      new JacksonTokenWriter(jsonFactory.createGenerator(writer))
-    }
-  }
+  ): JacksonTokenWriterProducer =
+    new JacksonTokenWriterProducer(jsonFactory, identity)
 
   implicit def jacksonTokenIteratorProducer(implicit
       jsonFactory: JsonFactory = defaultJsonFactory
   ): TokenIteratorProducer = new TokenIteratorProducer {
+    override def produce(json: String): Either[ReaderError, TokenIterator] =
+      ReaderError.catchNonFatal(
+        JacksonTokenIterator.fromFreshParser(jsonFactory.createParser(json))
+      )(FieldName.Root)
+
     override def fromReader(
         reader: Reader
     ): Either[ReaderError, TokenIterator] = {
       ReaderError.catchNonFatal(
         JacksonTokenIterator.fromFreshParser(jsonFactory.createParser(reader))
-      )(FieldName())
+      )(FieldName.Root)
     }
   }
 }
