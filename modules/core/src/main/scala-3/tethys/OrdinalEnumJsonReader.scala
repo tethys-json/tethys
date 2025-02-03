082127bb1adb7fa599065428cb6761da0f784a4a
Georgii Kovalev <goshacodes@gmail.com>
2025-02-03 11:44:47 +0500

    not important changes like make lazy val plain val

--- modules/core/src/main/scala-3/tethys/OrdinalEnumJsonReader.scala
+++ modules/core/src/main/scala-3/tethys/OrdinalEnumJsonReader.scala
@@ -3,20 +3,21 @@ package tethys
 import tethys.readers.{FieldName, ReaderError}
 import tethys.readers.tokens.TokenIterator
 
-trait OrdinalEnumJsonReader[A] extends JsonReader[A]
+class OrdinalEnumJsonReader[A <: scala.reflect.Enum](getByOrdinal: Int => A)
+    extends JsonReader[A]:
+  override def read(it: TokenIterator)(using FieldName): A =
+    if it.currentToken().isNumberValue then
+      val res = it.int()
+      it.next()
+      try getByOrdinal(res)
+      catch
+        case ex: NoSuchElementException =>
+          ReaderError.wrongJson(s"Unknown enum ordinal: $res")
+    else
+      ReaderError.wrongJson(
+        s"Expected int value but found: ${it.currentToken()}"
+      )
 
 object OrdinalEnumJsonReader:
   inline def derived[A <: scala.reflect.Enum]: OrdinalEnumJsonReader[A] =
-    new OrdinalEnumJsonReader[A]:
-      def read(it: TokenIterator)(implicit fieldName: FieldName): A =
-        if it.currentToken().isNumberValue then
-          val res = it.int()
-          it.next()
-          try derivation.EnumCompanion.getByOrdinal[A](res)
-          catch
-            case ex: NoSuchElementException =>
-              ReaderError.wrongJson(s"Unknown enum ordinal: $res")
-        else
-          ReaderError.wrongJson(
-            s"Expected int value but found: ${it.currentToken()}"
-          )
+    new OrdinalEnumJsonReader[A](derivation.EnumCompanion.getByOrdinal[A])
--- modules/core/src/main/scala/tethys/readers/KeyReader.scala
+++ modules/core/src/main/scala/tethys/readers/KeyReader.scala
@@ -5,33 +5,33 @@ trait KeyReader[A] {
 }
 
 object KeyReader {
-  implicit lazy val stringKeyReader: KeyReader[String] = new KeyReader[String] {
+  implicit val stringKeyReader: KeyReader[String] = new KeyReader[String] {
     override def read(s: String)(implicit fieldName: FieldName): String = s
   }
 
-  implicit lazy val uuidKeyReader: KeyReader[java.util.UUID] =
+  implicit val uuidKeyReader: KeyReader[java.util.UUID] =
     new KeyReader[java.util.UUID] {
       override def read(s: String)(implicit
           fieldName: FieldName
       ): java.util.UUID = java.util.UUID.fromString(s)
     }
 
-  implicit lazy val intKeyReader: KeyReader[Int] = new KeyReader[Int] {
+  implicit val intKeyReader: KeyReader[Int] = new KeyReader[Int] {
     override def read(s: String)(implicit fieldName: FieldName): Int = s.toInt
   }
 
-  implicit lazy val longKeyReader: KeyReader[Long] = new KeyReader[Long] {
+  implicit val longKeyReader: KeyReader[Long] = new KeyReader[Long] {
     override def read(s: String)(implicit fieldName: FieldName): Long = s.toLong
   }
 
-  implicit lazy val instantKeyReader: KeyReader[java.time.Instant] =
+  implicit val instantKeyReader: KeyReader[java.time.Instant] =
     new KeyReader[java.time.Instant] {
       override def read(s: String)(implicit
           fieldName: FieldName
       ): java.time.Instant = java.time.Instant.parse(s)
     }
 
-  implicit lazy val localDateKeyReader: KeyReader[java.time.LocalDate] =
+  implicit val localDateKeyReader: KeyReader[java.time.LocalDate] =
     new KeyReader[java.time.LocalDate] {
       override def read(
           s: String
@@ -40,7 +40,7 @@ object KeyReader {
           .parse(s, java.time.format.DateTimeFormatter.ISO_LOCAL_DATE)
     }
 
-  implicit lazy val localDateTimeKeyReader: KeyReader[java.time.LocalDateTime] =
+  implicit val localDateTimeKeyReader: KeyReader[java.time.LocalDateTime] =
     new KeyReader[java.time.LocalDateTime] {
       override def read(
           s: String
@@ -49,8 +49,7 @@ object KeyReader {
           .parse(s, java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME)
     }
 
-  implicit lazy val offsetDateTimeKeyReader
-      : KeyReader[java.time.OffsetDateTime] =
+  implicit val offsetDateTimeKeyReader: KeyReader[java.time.OffsetDateTime] =
     new KeyReader[java.time.OffsetDateTime] {
       override def read(
           s: String
@@ -59,7 +58,7 @@ object KeyReader {
           .parse(s, java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME)
     }
 
-  implicit lazy val zonedDateTimeKeyReader: KeyReader[java.time.ZonedDateTime] =
+  implicit val zonedDateTimeKeyReader: KeyReader[java.time.ZonedDateTime] =
     new KeyReader[java.time.ZonedDateTime] {
       override def read(
           s: String
