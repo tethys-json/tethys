b0bba048a66071f8c240fe1ce47ebbfe5cc14175
Aleksey Troitskiy <amtroitskiy@gmail.com>
2025-05-28 17:47:21 +0300

    fix warn about anon class duplicaton

--- modules/core/src/main/scala-3/tethys/OrdinalEnumJsonReader.scala
+++ modules/core/src/main/scala-3/tethys/OrdinalEnumJsonReader.scala
@@ -6,17 +6,22 @@ import tethys.readers.tokens.TokenIterator
 trait OrdinalEnumJsonReader[A] extends JsonReader[A]
 
 object OrdinalEnumJsonReader:
-  inline def derived[A <: scala.reflect.Enum]: OrdinalEnumJsonReader[A] =
+  def from[A](
+      impl: TokenIterator => FieldName ?=> A
+  ): OrdinalEnumJsonReader[A] =
     new OrdinalEnumJsonReader[A]:
-      def read(it: TokenIterator)(implicit fieldName: FieldName): A =
-        if it.currentToken().isNumberValue then
-          val res = it.int()
-          it.next()
-          try derivation.EnumCompanion.getByOrdinal[A](res)
-          catch
-            case ex: NoSuchElementException =>
-              ReaderError.wrongJson(s"Unknown enum ordinal: $res")
-        else
-          ReaderError.wrongJson(
-            s"Expected int value but found: ${it.currentToken()}"
-          )
+      def read(it: TokenIterator)(implicit fieldName: FieldName): A = impl(it)
+
+  inline def derived[A <: scala.reflect.Enum]: OrdinalEnumJsonReader[A] =
+    OrdinalEnumJsonReader.from[A]: it =>
+      if it.currentToken().isNumberValue then
+        val res = it.int()
+        it.next()
+        try derivation.EnumCompanion.getByOrdinal[A](res)
+        catch
+          case ex: NoSuchElementException =>
+            ReaderError.wrongJson(s"Unknown enum ordinal: $res")
+      else
+        ReaderError.wrongJson(
+          s"Expected int value but found: ${it.currentToken()}"
+        )
